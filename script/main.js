cb68c967fca5eac72c0a015783331a4a14bbe09c
そうまーる <soumar@soumarunoMacBook-Pro.local>
2022-03-14 10:50:23 +0900

    new modify

--- script/main.js
+++ script/main.js
@@ -4,12 +4,16 @@ const input_weight = document.getElementsByName("weight")[0];
 const data_list = document.getElementsByClassName("yourdata_list")[0];
 const register = document.getElementsByClassName("register")[0];
 
+if(isNaN(input_calorie) || isNaN(output_calorie) || isNaN(input_weight) ){
+    alert("Don't put character, put number");
+} 
+
 //make register button
 const registerDate = (input, output, weight) => {
     const listData = document.createElement("li");
     const showItem = data_list.appendChild(listData);
     var today = new Date();
-    var month = today.getMonth();
+    var month = today.getMonth()+1;
     var date = today.getDate();
     let yourdates = month+"/"+date+" Input calorie:"+input.value+"cal Output calorie:"+output.value+"cal Weight:"+weight.value+"k";
     showItem.innerHTML = yourdates;
@@ -30,33 +34,19 @@ const deleteTasks = (deleteButton) => {
     
 
 register.addEventListener("click", evt => {
-    registerDate(input_calorie, output_calorie, input_weight);
-    input_calorie.value = "";
-    output_calorie.value = "";
-    input_weight.value = "";
+    if(isNum(output_calorie) || isNum(input_calorie) || isNum(input_weight)){
+        alert("Please write number");
+        input_calorie.value = "";
+        output_calorie.value = "";
+        input_weight.value = "";
+    } else {
+        registerDate(input_calorie, output_calorie, input_weight);
+        input_calorie.value = "";
+        output_calorie.value = "";
+        input_weight.value = "";
+    }    
 });
-// const addTasks = (task) => {
-// 　　// 入力したタスクを追加・表示
-//     const listItem = document.createElement('li');
-//     const showItem = taskList.appendChild(listItem);
-//     showItem.innerHTML = task;
 
-// 　　//タスクに削除ボタンを付与
-//     const deleteButton = document.createElement('button');
-//     deleteButton.innerHTML = 'Delete';
-//     listItem.appendChild(deleteButton);
-
-// 　　//削除ボタンをクリックし、イベントを発動（タスクが削除）
-//     deleteButton.addEventListener('click', evt => {
-//         evt.preventDefault();
-//         deleteTasks(deleteButton);
-//     });
-// };
-
-
-// taskSubmit.addEventListener('click', evt => {
-//     evt.preventDefault();
-//     const task = taskValue.value;
-//     addTasks(task);
-//     taskValue.value = '';
-// });
+function isNum(val){
+    return !isNaN(val);
+}
